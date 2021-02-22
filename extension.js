4e61b34ac2870fdd536596de4fbaba688d441cc9
Justin Donnelly <24846452+justinrdonnelly@users.noreply.github.com>
2021-02-21 20:26:33 -0500

    more style conventions (https://wiki.gnome.org/Projects/GnomeShell/StyleGuide, https://gjs.guide/guides/gjs/style-guide.html)

--- extension.js
+++ extension.js
@@ -1,61 +1,44 @@
-/* extension.js
- *
- */
-
-/* exported init */
-
 'use strict';
 
+const Clutter = imports.gi.Clutter;
 const Lang = imports.lang;
 const Main = imports.ui.main;
 const Mainloop = imports.mainloop;
 const St = imports.gi.St;
-const Clutter = imports.gi.Clutter;
 
 class Extension {
+    //// Public methods ////
+    // instantiate the extension
     constructor() {
         //log("entering constructor");
-        this.monitor = Main.layoutManager.primaryMonitor;
-        this.size = 1; // size of the pixel
-        this.delay = 60; // how many seconds to take for the pixel to move across the entire screen
-        this.goLeft = null; // whether or not to go left next time we change directions
-        this.pixel = null; // the pixel
+        this._monitor = Main.layoutManager.primaryMonitor;
+        this._size = 1; // size of the pixel
+        this._delay = 60; // how many seconds to take for the pixel to move across the entire screen
+        this._goLeft = null; // whether or not to go left next time we change directions
+        this._pixel = null; // the pixel
         this._timeout = null; // timeout for when to change directions
         //log("exiting constructor");
     }
 
-    _changeDirection() {
-        //log("entering _changeDirection");
-        let newX = (this.goLeft) ? 0 : this.monitor.width - this.size;
-
-        if (this.pixel !== null) {
-            this.pixel.ease({
-                x: newX
-            });
-            this.goLeft = !this.goLeft;
-            this._timeout = Mainloop.timeout_add_seconds(this.delay, Lang.bind(this, this._changeDirection));
-        }
-        //log("exiting _changeDirection");
-    }
-
+    // enable the extension
     enable() {
         //log("entering enable");
-        this.goLeft = false; // start at left, so go right first
+        this._goLeft = false; // start at left, so go right first
         // instantiate a container, but we won't be putting an actor in it
-        this.pixel = new St.Bin({
+        this._pixel = new St.Bin({
             style: 'background-color: #101010', // #101010 - very slightly less dark than the top bar
             x: 0, //start at left
             y: 0,
             reactive: false,
             can_focus: false,
             track_hover: false,
-            width: this.size,
-            height: this.size,
+            width: this._size,
+            height: this._size,
         });
-        this.pixel.set_easing_duration(this.delay * 1000);
-        this.pixel.set_easing_mode(Clutter.AnimationMode.LINEAR);
+        this._pixel.set_easing_duration(this._delay * 1000);
+        this._pixel.set_easing_mode(Clutter.AnimationMode.LINEAR);
 
-        Main.layoutManager.addChrome(this.pixel, {
+        Main.layoutManager.addChrome(this._pixel, {
             affectsInputRegion: true,
             trackFullscreen: true,
         });
@@ -65,16 +48,34 @@ class Extension {
         //log("exiting enable");
     }
 
+    // disable the extension
     disable() {
         //log("entering disable");
         if (this._timeout !== null) {
             Mainloop.source_remove(this._timeout);
             this._timeout = null;
         }
-        Main.layoutManager.removeChrome(this.pixel);
-        this.pixel = null;
+        Main.layoutManager.removeChrome(this._pixel);
+        this._pixel = null;
         //log("exiting disable");
     }
+
+    //// Private methods ////
+    // change the direction of the pixel
+    _changeDirection() {
+        //log("entering _changeDirection");
+        const newX = (this._goLeft) ? 0 : this._monitor.width - this._size;
+
+        if (this._pixel !== null) {
+            this._pixel.ease({
+                x: newX
+            });
+            this._goLeft = !this._goLeft;
+            this._timeout = Mainloop.timeout_add_seconds(this._delay, Lang.bind(this, this._changeDirection));
+        }
+        //log("exiting _changeDirection");
+    }
+
 }
 
 function init() {
